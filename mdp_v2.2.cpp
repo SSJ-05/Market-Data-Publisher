016ad1b83daf1df725164b6f5ebd872588ba2613
ZeroK_Tech <149679147+SSJ-05@users.noreply.github.com>
2026-06-10 23:30:56 +0530

    Add files via upload

---
+++ mdp_v2.2.cpp
@@ -0,0 +1,178 @@
+// market data producer consumer mdp v2// 08.06.26// ZeroK
+
+/* workflow: producer thread -> ring buffer -> consumer thread -> stdout 
+ * throughput : 47M ticks/s
+ * bottleneck : memory ordering (irreductible)
+ * CPU : AMD Ryzen 3
+ * */
+
+#include <cstdint>
+#include <cstdlib>
+#include <cstdio>
+#include <cinttypes>    // for PRIu64/32
+#include <cstring>
+
+#include <random>       // for std::mt19937
+#include <chrono>
+
+#include <thread>
+#include <atomic>
+
+#include <type_traits>
+#include <immintrin.h>  // for _mm_pause
+#include <signal.h>
+
+// #include "ring_buffer_v2.1.hpp"
+#include "ring_buffer_v3.hpp"   // cached head and tail
+#include "thread_pinning.hpp"
+
+
+// for graceful shutdown on ctrl + c
+volatile sig_atomic_t RUNNING { 1 };
+void sig_handler (int sig) { RUNNING = 0; }
+
+
+
+// tick struct
+struct alignas(64) Tick {
+
+    std::uint64_t seq;              // client needs sequence number to detect packet loss
+    std::uint64_t timestamp_ns;     // time when tick generated
+
+    double bid;
+    double ask;
+
+    std::uint32_t bid_qty;
+    std::uint32_t ask_qty;
+
+    char symbol[8];
+    char pad[16];                   // make the size of Tick to 64 byte
+
+};
+
+
+
+constexpr int MAX_SIZE { 0x400 };   // 1024
+alignas(64) zerok::z_ring<Tick, MAX_SIZE> ring;
+
+alignas(64) std::atomic<std::uint64_t> produced { 0 };
+char pad0 [64 - sizeof(std::atomic<std::uint64_t>)]; 
+
+alignas(64) std::atomic<std::uint64_t> prev_produced { 0 };
+char pad1 [64 - sizeof(std::atomic<std::uint64_t>)]; 
+
+alignas(64) std::atomic<std::uint64_t> consumed { 0 };
+char pad2 [64 - sizeof(std::atomic<std::uint64_t>)]; 
+
+alignas(64) std::atomic<std::uint64_t> prev_consumed { 0 };
+char pad3 [64 - sizeof(std::atomic<std::uint64_t>)]; 
+
+
+// market data generator MDG class 
+class MDG {
+private:
+    std::uint64_t seq_;
+
+public:
+    MDG() : seq_(0) {}
+
+    Tick generate();
+};
+
+
+
+// generator logic
+Tick MDG::generate() {
+    Tick tick {};
+    tick.seq = ++seq_;
+
+    return tick;
+}
+
+
+// producer thread
+void producer () {
+    
+    pin_thread (0);
+
+    MDG gen;
+    while (RUNNING) {
+        Tick tick = gen.generate ();    // generate unconditionally
+
+        while (!ring.push(tick)) {
+            if (!RUNNING) return;
+            _mm_pause();
+        }
+        
+        produced.fetch_add (1, std::memory_order_relaxed);
+    }
+}
+
+
+// consumer thread
+void consumer () {
+    
+    pin_thread (1);
+
+    Tick tick;
+    std::uint64_t expected { 1 };
+
+    while (RUNNING || ring.empty()) {       // drain remaining ticks after shutdown
+        while (!ring.pop(tick)) {
+            if (!RUNNING && ring.empty()) return;
+            _mm_pause();
+        }
+
+        if (tick.seq != expected) {
+            std::printf ("gap expected : %" PRIu64
+                         "got : %" PRIu64 "\n\n", expected, tick.seq);
+
+            expected = tick.seq + 1;
+        }
+        else ++expected;
+
+        consumed.fetch_add (1, std::memory_order_relaxed);  
+    }
+}
+
+int main () {
+    // check if Ticks can be blasted thru network
+    static_assert (std::is_trivially_copyable_v<Tick>, "Tick must be blit-able");
+
+    signal (SIGINT,  sig_handler);
+    signal (SIGTERM, sig_handler);
+
+    std::printf("size of tick : %zu\n", sizeof(Tick));
+
+
+    std::thread pt (producer);
+    std::thread ct (consumer);
+
+    while (RUNNING) {
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        
+        auto prod_now = produced.load (std::memory_order_relaxed);
+        auto cons_now = consumed.load (std::memory_order_relaxed);
+
+        auto prod_rate = prod_now - prev_produced.load (std::memory_order_relaxed);
+        auto cons_rate = cons_now - prev_consumed.load (std::memory_order_relaxed);
+
+        prev_produced.store (prod_now, std::memory_order_relaxed);
+        prev_consumed.store (cons_now, std::memory_order_relaxed);
+
+   
+        std::printf ("produced/s : %" PRIu64 "\n"
+                     "consumed/s : %" PRIu64 "\n"
+                     "depth      : %zu\n\n",
+                    prod_rate,
+                    cons_rate,
+                    ring.size()
+                );
+    }
+
+    pt.join();
+    ct.join();
+
+    return EXIT_SUCCESS;
+}
